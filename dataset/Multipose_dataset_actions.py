af4d98c146abab689f4895a145714792d7cf2f60
jochen <jochen@mariekerke.be>
2018-05-10 13:03:15 +0200

    set dataset actions ready

--- dataset/Multipose_dataset_actions.py
+++ dataset/Multipose_dataset_actions.py
@@ -1,204 +1,31 @@
 import glob
 import logging
 import os
-
+import sys
+import cv2
+import common
+from urbanscene import features
+import matching
 import common
 import posematching.multi_person as multiperson
 import posematching.multi_person
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
+
 logger = logging.getLogger("Multipose dataset")
 
-multipose = '/media/jochen/2FCA69D53AB1BFF41/dataset/Multipose/fotos/'
-poses = '/media/jochen/2FCA69D53AB1BFF41/dataset/Multipose/poses/'
-data = '/media/jochen/2FCA69D53AB1BFF41/dataset/Multipose/json/'
-poses = '/media/jochen/2FCA69D53AB1BFF41/dataset/Multipose/poses/'
-
-galabal = '/media/jochen/2FCA69D53AB1BFF41/dataset/galabal2018/poses/'
-galabaljson = '/media/jochen/2FCA69D53AB1BFF41/dataset/galabal2018/json/'
-galabalfotos = '/media/jochen/2FCA69D53AB1BFF41/dataset/galabal2018/fotos/'
-
-
-
-#pose should look like 00100
-def find_matches_with(pose):
-
-    global eucl_dis_tresh_torso
-    global rotation_tresh_torso
-    global eucl_dis_tresh_legs
-    global rotation_tresh_legs
-    global eucld_dis_shoulders_tresh
-
-    eucl_dis_tresh_torso= 0.18
-    rotation_tresh_torso= 19
-    eucl_dis_tresh_legs= 0.058
-    rotation_tresh_legs= 24
-    eucld_dis_shoulders_tresh= 0.125
-    if len(pose) == 5 and pose.isdigit():
-        model = data+pose+"_keypoints.json"
-        model_features = common.parse_JSON_multi_person(model)
-        count = 0
-        os.system("mkdir -p "+poses+pose)
-        os.system("mkdir -p "+poses+pose+"/json")
-        os.system("mkdir -p "+poses+pose+"/jsonfout")
-        os.system("mkdir -p "+poses+pose+"/fotos")
-        os.system("mkdir -p "+poses+pose+"/fotosfout")
-        for json in glob.iglob(data+"*_keypoints.json"):
-            logger.info(json)
-            input_features = common.parse_JSON_multi_person(json)
-            (result, error_score, input_transform,something) = multiperson.match(model_features, input_features, normalise=True)
-            if result == True:
-                place = json.split("_keypoints")[0]
-                place = place.split("json/")[1]
-                place = place+".json"
-                os.system("cp "+json+" "+poses+pose+"/json/"+place)
-                foto = json.split("_keypoints")[0];
-                foto = foto.replace("json","fotos")
-                foto = foto +".jpg"
-                os.system("cp "+foto+" "+poses+pose+"/fotos/")
-                count = count+1
-                logger.info("true")
-        print ("there are "+str(count)+" matches found")
-
-    else:
-        print ("find_matches_with has wrong input")
-
-def test_script():
-    pose = "00100"
-    model = poses+pose+"/json/"+pose+".json"
-    model_features = common.parse_JSON_multi_person(model)
-    input = poses+pose+"/json/01179.json"
-
-    global eucl_dis_tresh_torso
-    global rotation_tresh_torso
-    global eucl_dis_tresh_legs
-    global rotation_tresh_legs
-    global eucld_dis_shoulders_tresh
-
-    eucl_dis_tresh_torso= 0.18
-    rotation_tresh_torso= 19
-    eucl_dis_tresh_legs= 0.058
-    rotation_tresh_legs= 24
-    eucld_dis_shoulders_tresh= 0.125
-
-    input_features = common.parse_JSON_multi_person(input)
-    (result, error_score, input_transform,something) = multiperson.match(model_features, input_features, normalise=True)
-    print (result)
-
-def check_matches(pose):
-    global eucl_dis_tresh_torso
-    global rotation_tresh_torso
-    global eucl_dis_tresh_legs
-    global rotation_tresh_legs
-    global eucld_dis_shoulders_tresh
-
-    eucl_dis_tresh_torso= 0.18
-    rotation_tresh_torso= 19
-    eucl_dis_tresh_legs= 0.058
-    rotation_tresh_legs= 24
-    eucld_dis_shoulders_tresh= 0.125
-    path = poses+pose
-    model = path+"/json/"+pose+".json"
-    #pose = "1"
-    #path = '/media/jochen/2FCA69D53AB1BFF41/dataset/poses/pose'+pose
-    #model = path+"/json/0.json"
-    model_features = common.parse_JSON_multi_person(model)
-
-    true_positive =0
-    false_positive =0
-    true_negative =0
-    false_negative =0
-
-    for json in glob.iglob(path+"/json/*.json"):
-        #print (json)
-        input_features = common.parse_JSON_multi_person(json)
-        (result, error_score, input_transform,something) = multiperson.match(model_features, input_features, True)
-        if result == True:
-            true_positive = true_positive +1
-        else:
-            false_negative = false_negative +1
-            print ("error at: "+json)
-
-    for json in glob.iglob(path+"/jsonfout/*.json"):
-        #print (json)
-        input_features = common.parse_JSON_multi_person(json)
-        (result, error_score, input_transform,something) = multiperson.match(model_features, input_features, True)
-        if result == True:
-            false_positive = false_positive +1
-            print ("error at: "+json)
-        else:
-            true_negative = true_negative +1
-
-    precision = 0
-    recall =0
-    if (true_positive+false_positive) !=0:
-        precision = true_positive / (true_positive+false_positive)
-    if  (true_positive+false_negative) !=0:
-        recall = true_positive / (true_positive+false_negative)
-
-    #******** print small raport ******************
-
-    print ("*************raport of pose "+pose+" ****************")
-    print ("true_positive: " + str(true_positive))
-    print ("false_positive: "+ str(false_positive))
-    print ("true_negative: " + str(true_negative))
-    print ("false_negative: "+ str(false_negative))
-    print ("recall: "+ str(recall))
-    print ("precision: "+ str(precision))
+schijfnaam = "/media/jochen/2FCA69D53AB1BFF43/"
+poses = schijfnaam+'dataset/kever/poses/'
+json = schijfnaam+'dataset/kever/json/'
+fotos = schijfnaam+'dataset/kever/fotos/'
 
 
 
 
-#***********************************galabal dataset_actions*********************************
-def find_galabal_matches(pose):
-    model = galabaljson+pose+".json"
-    model_features = common.parse_JSON_multi_person(model)
-    count = 0
-    os.system("mkdir -p "+galabal+pose)
-    os.system("mkdir -p "+galabal+pose+"/json")
-    os.system("mkdir -p "+galabal+pose+"/jsonfout")
-    os.system("mkdir -p "+galabal+pose+"/fotos")
-    os.system("mkdir -p "+galabal+pose+"/fotosfout")
-    for json in glob.iglob(galabaljson+"*.json"):
-        logger.info(json)
-        input_features = common.parse_JSON_multi_person(json)
-        (result, error_score, input_transform,something) = multiperson.match(model_features, input_features, normalise=True)
-        if result == True:
-            place = json.split(".json")[0]
-            place = place.split("json/")[1]
-            place = place+".json"
-            os.system("cp "+json+" "+galabal+pose+"/json/"+place)
-            foto = json.split(".json")[0];
-            foto = foto.replace("json","fotos")
-            foto = foto +".jpg"
-            os.system("cp "+foto+" "+galabal+pose+"/fotos/")
-            count = count+1
-            logger.info("true")
-    print ("there are "+str(count)+" matches found")
-
-def check_galabal_matches(pose):
-    model = galabal+pose+"/json/"+pose+".json"
-    model_features = common.parse_JSON_multi_person(model)
-    count =0
-    for json in glob.iglob(galabal+pose+"/json/*.json"):
-        logger.info(json)
-        input_features = common.parse_JSON_multi_person(json)
-        (result, error_score, input_transform,something) = multiperson.match(model_features, input_features, normalise=True)
-        if result == False:
-            count = count +1
-            print ("error at: "+json)
-    print (str(count)+" foto's werden niet meer herkend")
 
 #*****************************************logic*********************************************
 def rename_files():
-    i=0
-    for json in glob.iglob(galabal+"*_keypoints.json"):
-        i = i+1
-        os.system("cp "+json+" "+galabal+str(i)+".json")
-        foto = json.split("_keypoints")[0];
-        foto = foto.replace("json","fotos")
-        foto = foto +".jpg"
-        os.system("cp "+foto+" "+galabalfotos+str(i)+".jpg")
+    print("not implemented")
 
 def replace_json_files(pose):
     for foto in glob.iglob(poses+pose+"/fotosfout/*"):
@@ -208,36 +35,37 @@ def replace_json_files(pose):
         os.system("mv "+foto+" "+poses+pose+"/jsonfout/  2>/dev/null")
 
 #**************************************precision recall********************************************
-def calculate_pr(pose):
+def calculate_pr(pose,tresh):
     path = poses+pose
     model = path+"/json/"+pose+".json"
     #pose = "1"
     #path = '/media/jochen/2FCA69D53AB1BFF41/dataset/poses/pose'+pose
     #model = path+"/json/0.json"
-    model_features = common.parse_JSON_multi_person(model)
-
+    model_pose_features = common.parse_JSON_multi_person(model)
+    feature_name = 'orb-flann'
+    detector, matcher = features.init_feature(feature_name)
+    model_image = cv2.imread(model.split(".")[0].replace("json","fotos")+".jpg", cv2.IMREAD_GRAYSCALE)
+    print(model.split(".")[0].replace("json","fotos")+".jpg")
     true_positive =0
     false_positive =0
     true_negative =0
     false_negative =0
 
     for json in glob.iglob(path+"/json/*.json"):
-        #print (json)
-        input_features = common.parse_JSON_multi_person(json)
-        (result, error_score, input_transform,something) = multiperson.match(model_features, input_features, True)
-        if result == True:
+        input_pose_features= common.parse_JSON_multi_person(json)
+        input_image = cv2.imread(json.split(".")[0].replace("json","fotos")+".jpg", cv2.IMREAD_GRAYSCALE)
+        result_whole = matching.match_whole(model_pose_features, input_pose_features, detector, matcher, model_image, input_image,False, False)
+        if result_whole < tresh:
             true_positive = true_positive +1
         else:
             false_negative = false_negative +1
-            print ("error at: "+json)
 
     for json in glob.iglob(path+"/jsonfout/*.json"):
-        #print (json)
-        input_features = common.parse_JSON_multi_person(json)
-        (result, error_score, input_transform,something) = multiperson.match(model_features, input_features, True)
-        if result == True:
+        input_pose_features= common.parse_JSON_multi_person(json)
+        input_image = cv2.imread(json.split(".")[0].replace("json","fotos")+".jpg", cv2.IMREAD_GRAYSCALE)
+        result_whole = matching.match_whole(model_pose_features, input_pose_features, detector, matcher, model_image, input_image,False, False)
+        if result_whole < tresh:
             false_positive = false_positive +1
-            print ("error at: "+json)
         else:
             true_negative = true_negative +1
 
@@ -261,53 +89,28 @@ def calculate_pr(pose):
     return (precision,recall)
 
 def make_pr_curve(pose):
-    global eucl_dis_tresh_torso
-    global rotation_tresh_torso
-    global eucl_dis_tresh_legs
-    global rotation_tresh_legs
-    global eucld_dis_shoulders_tresh
-    '''
-    eucl_dis_tresh_torso= 0.098
-    rotation_tresh_torso= 10.847
-    eucl_dis_tresh_legs= 0.05
-    rotation_tresh_legs= 14.527
-    eucld_dis_shoulders_tresh= 0.085
-    
-    SP_DISTANCE_TORSO = 0.18 # 0.098
-    SP_ROTATION_TORSO = 19
-
-    SP_DISTANCE_LEGS = 0.058
-    SP_ROTATION_LEGS = 24   # 14.527
-
-    SP_DISTANCE_SHOULDER = 0.125
-    '''
-
-
-    start_eucl_dis_tresh_torso= 0.0 #=>0.13
-    start_rotation_tresh_torso=  20#=> 14
-    start_eucl_dis_tresh_legs= 0.08 #=> 0.09
-    start_rotation_tresh_legs= 25#=>18
-    start_eucld_dis_shoulders_tresh= 0.2 #=>0.12
-    precisions = [];
+    precisions = []
     recalls = []
-    for i in range(0,1000):
-        eucl_dis_tresh_torso= start_eucl_dis_tresh_torso +(i*0.001)
-        rotation_tresh_torso= start_rotation_tresh_torso +(i*0.1)
-        eucl_dis_tresh_legs= start_eucl_dis_tresh_legs +(i*0.001)
-        rotation_tresh_legs= start_rotation_tresh_legs +(i*0.1)
-        eucld_dis_shoulders_tresh= start_eucld_dis_shoulders_tresh +(i*0.001)
-        (precision,recall) = calculate_pr(pose)
+    start_tresh = 0
+    for i in range(0,10):
+        tresh = start_tresh + i*0.1
+        (precision,recall) = calculate_pr(pose,tresh)
         precisions.append(precision)
         recalls.append(recall)
 
-    draw_pr_curve(precisions,recalls, pose)
+    return(precisions,recalls)
 
 
-def draw_pr_curve(precision, recall, pose):
-    plt.plot(recall,precision)
+def draw_pr_curve():
+    pose = "29"
+    precisions, recalls = make_pr_curve(pose)
+
+    plt.plot(recalls,precisions, label="describing poses")
+
+    plt.legend(loc=3, ncol=2, mode="expand", borderaxespad=0.)
     plt.ylabel('precision')
     plt.xlabel('recall')
-    plt.title('Pr curve of pose : '+pose)
+    plt.title('Pr curves of urban algorithm')
     plt.axis([0,1,0,1])
     plt.legend()
 
--- jochen_deel.py
+++ jochen_deel.py
@@ -4,45 +4,9 @@ import logging
 
 import dataset.Multipose_dataset_actions as dataset
 logger = logging.getLogger(__name__)
-path = '/media/jochen/2FCA69D53AB1BFF49/dataset/poses/'
 
-'''
-logging.basicConfig(level=logging.INFO)
-for i in range(1,10):
-    print "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-    print i
-    print "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-    Multipose_dataset_actions.find_matches_with("0000"+str(i))
-'''
-
-'''
-logging.basicConfig(level=logging.ERROR)
-dataset.find_matches_with("00100")
-'''
-'''
-logging.basicConfig(level=logging.CRITICAL)
-dataset.check_matches("00100")
-'''
-'''
-logging.basicConfig(level=logging.DEBUG)
-dataset.test_script()
-'''
-#*********************galabal*********************
-
-'''
-logging.basicConfig(level=logging.DEBUG)
-dataset.test_script()
-'''
-'''
-logging.basicConfig(level=logging.INFO)
-dataset.find_galabal_matches("1")
-'''
-'''
-logging.basicConfig(level=logging.DEBUG)
-dataset.check_galabal_matches("4")
-'''
 
 #**************************pr curves*********************
 
 logging.basicConfig(level=logging.ERROR)
-dataset.make_pr_curve("00100")
+dataset.draw_pr_curve()
