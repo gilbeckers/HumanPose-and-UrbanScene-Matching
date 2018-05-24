37d052c38c50cc9be9caf0c5f94102b002d0053d
jochen <jochen@mariekerke.be>
2018-05-24 15:22:19 +0200

    plots voor jochen in jochen_deel

--- jochen_deel.py
+++ jochen_deel.py
@@ -1,48 +1,81 @@
-
+import sys
+import cv2
+import common
+from urbanscene import features
 import logging
-import logging
-
-import dataset.Multipose_dataset_actions as dataset
-logger = logging.getLogger(__name__)
-path = '/media/jochen/2FCA69D53AB1BFF49/dataset/poses/'
-
-'''
-logging.basicConfig(level=logging.INFO)
-for i in range(1,10):
-    print "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-    print i
-    print "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-    Multipose_dataset_actions.find_matches_with("0000"+str(i))
-'''
-
-'''
-logging.basicConfig(level=logging.ERROR)
-dataset.find_matches_with("00100")
-'''
-'''
-logging.basicConfig(level=logging.CRITICAL)
-dataset.check_matches("00100")
-'''
-'''
+import matching
+from matplotlib import pyplot as plt
+import timer
 logging.basicConfig(level=logging.DEBUG)
-dataset.test_script()
-'''
-#*********************galabal*********************
+logger = logging.getLogger("main")
+import plot_vars
 
-'''
-logging.basicConfig(level=logging.DEBUG)
-dataset.test_script()
-'''
-'''
-logging.basicConfig(level=logging.INFO)
-dataset.find_galabal_matches("1")
-'''
-'''
-logging.basicConfig(level=logging.DEBUG)
-dataset.check_galabal_matches("4")
-'''
 
-#**************************pr curves*********************
+feature_name = 'orb-flann'
+#path_img = '../img/galabal2018/fotos/'    #'../img/'  #'posesGeoteam/fotos/'
+#path_json = '../img/galabal2018/json/' #'../json_data/'   #'posesGeoteam/json/'
+schijfnaam = "/media/jochen/2FCA69D53AB1BFF43/"
+poses = schijfnaam+'dataset/kever/poses/'
+urban_json = schijfnaam+'dataset/kever/json/'
+urban_fotos = schijfnaam+'dataset/kever/fotos/'
 
-logging.basicConfig(level=logging.ERROR)
-dataset.make_pr_curve("00100")
+
+path_img = urban_fotos  # 'posesGeoteam/fotos/'
+path_json = urban_json  # 'posesGeoteam/json/'
+
+#path_img = "../img/"
+#path_json = '../json_data/'  # 'posesGeoteam/json/'
+
+model_name = '17.jpg' #'bulb14.jpg'  # goeie : "pisa9"  taj3  # trap1     trap1
+input_name = '127.jpg' #'bulb16.jpg' # goeie : "pisa10"  taj4  # trap2     trap3
+
+#model_name="duo21.jpg"
+#input_name="duo39.jpg"
+
+model_image = cv2.imread(path_img + model_name, cv2.IMREAD_GRAYSCALE)
+input_image = cv2.imread(path_img + input_name, cv2.IMREAD_GRAYSCALE)
+
+detector, matcher = features.init_feature(feature_name)
+
+if model_image is None:
+    print('Failed to load fn1:', model_name)
+    sys.exit(1)
+
+if input_image is None:
+    print('Failed to load fn2:', input_image)
+    sys.exit(1)
+
+if detector is None:
+    print('unknown feature:', feature_name)
+    sys.exit(1)
+
+logger.debug(" using %s", feature_name)
+
+include_keypoints = False
+plot_us = True  # plot urban scene
+plot_mp = False # plot multi pose
+plot_vars.input_name = input_name
+plot_vars.model_name = model_name
+plot_vars.model_path = path_img + model_name
+plot_vars.input_path = path_img + input_name
+plot_vars.write_img = False
+global crop
+global correction
+crop = True
+correction = True
+
+
+logger.debug("---Starting pose matching --")
+if include_keypoints:
+    model_pose_features = common.parse_JSON_multi_person(path_json + model_name.split('.')[0] + '_keypoints' +  '.json')  # + '_keypoints'
+    input_pose_features = common.parse_JSON_multi_person(path_json + input_name.split('.')[0]  +'_keypoints' + '.json')
+else:
+    model_pose_features = common.parse_JSON_multi_person(path_json + model_name.split('.')[0].replace('fotos','json') +  '.json')  # + '_keypoints'
+    input_pose_features = common.parse_JSON_multi_person(path_json + input_name.split('.')[0].replace('fotos','json')  +'.json')
+
+
+
+result_whole = matching.match_whole(model_pose_features, input_pose_features, detector, matcher, model_image, input_image,plot_us, plot_mp)
+
+if plot_us or plot_mp:
+    plt.show()
